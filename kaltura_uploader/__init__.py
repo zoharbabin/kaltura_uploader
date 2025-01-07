d4db2810768c25341a0a8309b2836f0f0efad1da
Zohar Babin <zohar.babin@kaltura.com>
2025-01-07 14:25:32 -0500

    clean

--- kaltura_uploader/__init__.py
+++
@@ -1,9 +0,0 @@
-# kaltura_uploader/__init__.py
-
-from .uploader import KalturaUploader
-from .logging_config import configure_logging
-
-__all__ = [
-    "KalturaUploader",
-    "configure_logging",
-]
--- tests/__init__.py
+++
@@ -1,3 +0,0 @@
-# tests/__init__.py
-
-# This file is intentionally left blank to indicate that 'tests' is a Python package.
